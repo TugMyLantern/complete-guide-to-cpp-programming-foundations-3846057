4e07eeac9359a8398aa906c3655c0930ad15aae5
TugMyLantern <67441354+TugMyLantern@users.noreply.github.com>
2026-07-20 08:36:11 +0000

    ch2-5

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -4,8 +4,29 @@
 
 #include <iostream>
 
+int a, b = 5;
+
 int main(){
     
-    std::cout << std::endl << std::endl;
+    bool my_flag = false;
+    a = 7;
+
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "flag = " << my_flag << std::endl;
+
+    my_flag = true;
+
+    std::cout << "flag = " << my_flag << std::endl;
+    std::cout << "a + b = " << a + b << std::endl;
+    std::cout << "b - a = " << b - a << std::endl;
+
+    unsigned int positive;
+
+    positive = b - a;
+    std::cout << "b - a (unsigned) = " << positive << std::endl;
+
+    std::cout << std::endl << "===" << std::endl << std::endl;
+
     return 0;
 }
